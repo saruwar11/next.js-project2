f4b2096038f7fd5c6f29a0c232430bb7a3e3c943
saruwar11 <saruwarhossain26@gmail..com>
2025-01-24 18:54:13 +0600

    edit profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,8 +1,13 @@
-const Profile = () => {
+import Image from "next/image";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const Profile = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
-    <div>
-      <h1>Welcome to your profile!</h1>
-      <p>You are logged in successfully.</p>
+    <div className="flex flex-col justify-center items-center p-4 bg-gray-100 min-h-screen">
+      <h1 className="mt-4 text-xl text-green-600 ">Welcome to your profile!</h1>
     </div>
   );
 };
