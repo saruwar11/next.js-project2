36aa3aa80615ef6cef6d4c20dd142dbe3cbc616f
saruwar11 <saruwarhossain26@gmail..com>
2025-01-24 20:31:01 +0600

    update all

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,10 +1,4 @@
-import Image from "next/image";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-
-const Profile = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-
+const Profile = () => {
   return (
     <div className="flex flex-col justify-center items-center p-4 bg-gray-100 min-h-screen">
       <h1 className="mt-4 text-xl text-green-600 ">Welcome to your profile!</h1>
